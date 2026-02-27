a4bbf0d50a80090801d82822af7d883487379cf3
ross0778 <ross0778@umn.edu>
2026-02-27 11:44:41 -0600

    Create vector with only non-blank lines from citations

--- R/week6.R
+++ R/week6.R
@@ -1,6 +1,13 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(stringi)
 #library call
 #library call
 #library call
-#library call
+
+# Data Import
+citations <- stri_read_lines("../data/cites.txt", encoding = "Windows-1252")
+citations_txt <- stri_subset_regex(citations, ".+")
+
+
+# Data Cleaning
