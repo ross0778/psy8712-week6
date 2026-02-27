42944e9db68ef9ada9d2157af9f0f51461586a93
ross0778 <ross0778@umn.edu>
2026-02-27 12:01:23 -0600

    Print text in data import stage

--- R/week6.R
+++ R/week6.R
@@ -1,13 +1,14 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
-#library call
+library(stringr)
 #library call
 #library call
 
 # Data Import
 citations <- stri_read_lines("../data/cites.txt", encoding = "Windows-1252")
 citations_txt <- stri_subset_regex(citations, ".+")
-
+print(str_c("The number of blank lines eliminated was ", length(citations) - length(citations_txt)))
+print(str_c("The average number of characters/citation was ", mean(nchar(citations_txt))))
 
 # Data Cleaning
